02ae9029beeee74cec886b62c41aea99beaaad97
Ka. Santana <sdk.santana@gmail.com>
2023-09-06 18:00:04 -0300

    exercicios

--- Aula01/src/exercicio01.java
+++ Aula01/src/exercicio01.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 public class exercicio01 {
     public static void main(String[] args) {
         Scanner textoDigitado = new Scanner(System.in);
-        System.out.println("Digite o seu nome: ");
-
+            System.out.println("Digite o seu nome: ");
         String seuNome = textoDigitado.nextLine();
-        System.out.println("Seu nome é: " + seuNome);
+            System.out.println("Seu nome é: " + seuNome);
     }
 }
