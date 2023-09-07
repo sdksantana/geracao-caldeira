ae573417c60e278a8d0fecf681404e6b1bec4f6c
Ka. Santana <sdk.santana@gmail.com>
2023-09-06 22:22:32 -0300

    exercicios

---
+++ Aula01/src/exercicio03.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class exercicio03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite três números");
+
+        int n = scanner.nextInt();
+        int maior = n;
+        int menor = n;
+        double soma = n;
+
+        for (int i = 1; i < 3; i++) {
+            n = scanner.nextInt();
+            soma += n;
+            if (n > maior)
+                maior = n;
+            if (n < menor)
+                menor = n;
+        }
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.println("Maior número: " + maior);
+        System.out.println("Menor número: " + menor);
+        System.out.println("Média: " + df.format(soma / 3));
+
+
+    }
+}
