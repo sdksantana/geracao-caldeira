bde0f82d2294e362323e91a85a931bc7fa0245bb
sdksantana <sdk.santana@gmail.com>
2023-09-19 16:12:59 -0300

    aulao4

---
+++ Aula04/src/Exercicio01/Veiculo.java
@@ -0,0 +1,12 @@
+package Exercicio01;
+
+public class Veiculo {
+    public String marca;
+    public String modelo;
+    public int ano;
+
+    public void calcularDistancia (int distancia);
+    {
+
+    }
+}
