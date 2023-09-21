e8c1346bd472b91126ed55281af4f61aaa42e0a2
Ka. Santana <sdk.santana@gmail.com>
2023-09-21 20:35:58 -0300

    exclusao

--- Aula04/src/com/github/aula04/Exercicio01/Carro.java
+++
@@ -1,3 +0,0 @@
-package com.github.aula04.Exercicio01;
-public abstract class Carro extends Veiculo {
-}
--- Aula04/src/com/github/aula04/Exercicio02/FormaGeometrica.java
+++
@@ -1,8 +0,0 @@
-package com.github.aula04.Exercicio02;
-
-public abstract class FormaGeometrica {
-    public abstract double calcularArea();
-}
-
-
-
