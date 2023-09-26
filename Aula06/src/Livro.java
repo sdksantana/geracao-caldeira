415fc4f8af37bd303cbb82bfbdd2fd423124127d
Ka. Santana <sdk.santana@gmail.com>
2023-09-26 14:54:12 -0300

    A06E01-Update

---
+++ Aula06/src/Livro.java
@@ -0,0 +1,20 @@
+public class Livro {
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    private String titulo;
+    private String categoria;
+}
