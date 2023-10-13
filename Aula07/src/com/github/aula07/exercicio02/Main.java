fb4c79539992146401e68c8ed81c98500ba8f374
Ka. Santana <sdk.santana@gmail.com>
2023-10-13 20:13:52 -0300

    Update

--- Aula07/src/com/github/aula07/exercicio02/Main.java
+++ Aula07/src/com/github/aula07/exercicio02/Main.java
@@ -1,4 +1,5 @@
 package com.github.aula07.exercicio02;
+import java.text.Normalizer;
 import java.util.Scanner;
 
 public class Main {
@@ -15,41 +16,47 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Que dia é hoje?");
-        String diaHoje = scanner.nextLine().toUpperCase();//Converte as strings digitadas para letras maiusculas, evitando erro de verificação
+        String diaHoje;
+        boolean entradaValida = false;
 
-        DiaDaSemana dia;
-        try {
-            dia = DiaDaSemana.valueOf(diaHoje);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Inválido. Digite um dia existente.");
-            return;
-        }
+        do {
+            System.out.println("Que dia é hoje?");
+            diaHoje = scanner.nextLine().toUpperCase();
+            // Remove a acentuação da entrada
+            diaHoje = Normalizer.normalize(diaHoje, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
 
-        switch (dia) {
-            case SEGUNDA:
-                System.out.println("Hoje é Segunda-feira.");
-                break;
-            case TERCA:
-                System.out.println("Hoje é Terça-feira.");
-                break;
-            case QUARTA:
-                System.out.println("Hoje é Quarta-feira.");
-                break;
-            case QUINTA:
-                System.out.println("Hoje é Quinta-feira.");
-                break;
-            case SEXTA:
-                System.out.println("Hoje é Sexta-feira.");
-                break;
-            case SABADO:
-                System.out.println("Hoje é Sábado.");
-                break;
-            case DOMINGO:
-                System.out.println("Hoje é Domingo.");
-                break;
-            default:
-                System.out.println("Inválido. Digite um dia existente.");
-        }
+            try {
+                DiaDaSemana dia = DiaDaSemana.valueOf(diaHoje);
+                switch (dia) {
+                    case SEGUNDA:
+                        System.out.println("Hoje é Segunda-feira.");
+                        break;
+                    case TERCA:
+                        System.out.println("Hoje é Terça-feira.");
+                        break;
+                    case QUARTA:
+                        System.out.println("Hoje é Quarta-feira.");
+                        break;
+                    case QUINTA:
+                        System.out.println("Hoje é Quinta-feira.");
+                        break;
+                    case SEXTA:
+                        System.out.println("Hoje é Sexta-feira.");
+                        break;
+                    case SABADO:
+                        System.out.println("Hoje é Sábado.");
+                        break;
+                    case DOMINGO:
+                        System.out.println("Hoje é Domingo.");
+                        break;
+                    default:
+                        System.out.println("Inválido. Digite um dia existente.");
+                        break;
+                }
+                entradaValida = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Inválido. Digite um dia existente.\n");
+            }
+        } while (!entradaValida);
     }
 }
--- Aula07/src/com/github/aula07/exercicio03/calculavel.java
+++
@@ -1,7 +0,0 @@
-package com.github.aula07.exercicio03;
-public interface calculavel {
-    public void multiplicar();
-    public void somar();
-    public void subtrair();
-    public void dividir();
-}
